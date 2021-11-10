2b7a75f2a0acf89fcff70c9615ba7ab7f5df1089
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-09 19:12:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+//grabber
+const inputField = document.getElementById('new-task-description')
+const list = document.getElementById('tasks')
+const form = document.getElementById('create-task-form')
+
+
+
+
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(event.target.input.value)
+});
